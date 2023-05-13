f78049837e9fc4965c6251215423ad3b5d84ddef
Max White <maxwhit1983@gmail.com>
2023-05-13 23:56:39 +0300

    fixes

--- src/main/java/com/robot/dreams/services/StudentsManager.java
+++ src/main/java/com/robot/dreams/services/StudentsManager.java
@@ -17,11 +17,13 @@ public class StudentsManager {
         Student student2 = new Student(22, "Bob Tailor", "physics-1e342343");
         Student student3 = new Student(21, "Anna Black", "literature-1342343");
         Student student4 = new Student(25, "John Wick", "cs-1e34er343");
+        Student student5 = new Student(25, "Ted Barton", "cs-r343");
 
         students.add(student1);
         students.add(student2);
         students.add(student3);
         students.add(student4);
+        students.add(student5);
 
         return students;
     }
